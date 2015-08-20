5085863baf6aafc63fa0e7c07f0fdb98c536ffe4
Danny Selgo <selgod@mail.gvsu.edu>
2015-08-20 16:51:14 -0400

    Changes to wireframe and added side-menu

---
+++ MtGLifeCounter/MtGLifeCounter/LeftSideViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LeftSideViewController.swift
+//  MtGLifeCounter
+//
+//  Created by Danny on 8/20/15.
+//  Copyright (c) 2015 Danny. All rights reserved.
+//
+
+//import Cocoa
+
+import UIKit
+
+class LeftSideViewController: UIViewController {
+
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning(){
+        super.didReceiveMemoryWarning()
+    }
+}
---
+++ MtGLifeCounter/MtGLifeCounter/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MtGLifeCounter
+//
+//  Created by Danny on 8/20/15.
+//  Copyright (c) 2015 Danny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func leftSideButtonTapped(sender: AnyObject) {
+        var appDelegate:AppDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        appDelegate.centerContainer!.toggleDrawerSide(MMDrawerSide.Left, animated: true, completion: nil)
+    }
+}
+
